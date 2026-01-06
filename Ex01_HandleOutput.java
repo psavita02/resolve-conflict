d45c09b451a6e8027aec09f6eec7a6be129ad7e9
psavita02 <priyanka.savita@telusinternational.com>
2026-01-06 21:19:08 +0530

    dev2 changes

--- Ex01_HandleOutput.java
+++ Ex01_HandleOutput.java
@@ -6,7 +6,7 @@ public class Ex01_HandleOutput
 public static void main(String[] args) {
 System.out.println(1);
 System.out.println(1.5);
-System.out.println('C');
+System.out.println('E');
 System.out.println("Hello World");
 System.out.println(true);
 System.out.println("******************************");
