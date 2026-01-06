8ad4673fa12cef3e60376dd7b28308528f7dc3a0
psavita02 <priyanka.savita@telusinternational.com>
2026-01-06 21:07:19 +0530

    resolve conflict file

---
+++ Ex01_HandleOutput.java
@@ -0,0 +1,15 @@
+
+package _01_basic;
+
+public class Ex01_HandleOutput 
+{
+public static void main(String[] args) {
+System.out.println(1);
+System.out.println(1.5);
+System.out.println('A');
+System.out.println("Hello World");
+System.out.println(true);
+System.out.println("******************************");
+}
+}
+
